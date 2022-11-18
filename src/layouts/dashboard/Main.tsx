f8e483c4e4a249f4a4a105f004b46817a3e681ec
Aleksandar Lakatos <aleksandar.lakatos@bluegrassdigital.com>
2022-11-18 12:54:59 +0100

    Super admin dashboard

--- src/layouts/dashboard/Main.tsx
+++ src/layouts/dashboard/Main.tsx
@@ -6,6 +6,7 @@ import useResponsive from '../../hooks/useResponsive';
 import { HEADER, NAV } from '../../config';
 // components
 import { useSettingsContext } from '../../components/settings';
+import { GREYS } from 'src/theme/palette';
 
 // ----------------------------------------------------------------------
 
@@ -45,14 +46,16 @@ export default function Main({ children, sx, ...other }: BoxProps) {
       sx={{
         flexGrow: 1,
         py: `${HEADER.H_MOBILE + SPACING}px`,
+        px: 3,
         ...(isDesktop && {
-          px: 2,
+          px: 5,
           py: `${HEADER.H_DASHBOARD_DESKTOP + SPACING}px`,
           width: `calc(100% - ${NAV.W_DASHBOARD}px)`,
           ...(isNavMini && {
             width: `calc(100% - ${NAV.W_DASHBOARD_MINI}px)`,
           }),
         }),
+        bgcolor: GREYS.grey5,
         ...sx,
       }}
       {...other}
